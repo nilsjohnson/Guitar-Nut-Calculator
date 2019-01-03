52fc4a307861e7bfd2b5542d5674019f65069689
Nils <nilsjohnson328@gmail.com>
2019-01-03 18:32:50 -0500

    removed custom set option, refactored calculation alorithm

--- js/script.js
+++ js/script.js
@@ -1,75 +1,47 @@
 var precision = 3;
 var currentSets = null;
 var chosenSet = null;
-var octaveInputShowing = false;
-var lastOctaveSpacing = 1;
-
-document.getElementById("octave").addEventListener('change', function() {
-    if(this.checked) 
-    {
-    	showOctaveSpacingInput();
-    }
-    else
-    {
-    	removeOctaveSpacingInput();
-    }
-});
-
-
-var isOctave = function()
-{
-	if(document.getElementById("octave").checked)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
+var isOctave = null;
 
 
 var setInstrument = function(value)
 {
-	document.getElementById("num-strings").readOnly = true;
-	document.getElementById("num-strings").classList.remove("focused");
-	document.getElementById("octave").checked = false;
-	document.getElementById("octave").disabled = true; 
-	removeOctaveSpacingInput();
+	var widthElement = document.getElementById("width");
 
 	switch(value)
 	{
 		case "guitar":
+			widthElement.value = "1.45";
 			fillStringSetList(guitarSets);
+			isOctave = false;
 			break;
 
 		case "bass":
+			widthElement.value = "1.00";
 			fillStringSetList(bassSets);
+			isOctave = false;
 			break;
-
-		case "custom":
-			document.getElementById("octave").disabled = false; 
-			document.getElementById("num-strings").readOnly = false;
-			document.getElementById("num-strings").classList.add("focused");
-			document.getElementById("num-strings").addEventListener("change", makeCustomSet);
-
-			fillStringSetList(custSets);
-
-			break;
-	
 
 		case "mandolin":
-			showOctaveSpacingInput();
+			widthElement.value = "1.00";
 			fillStringSetList(mandoSets);
-			document.getElementById("octave").checked = true;
-	
-			break;	
+			isOctave = true
+			break;
+
+		case "12-string":
+			widthElement.value = "1.75";
+			fillStringSetList(twelveStringSets);
+			isOctave = true
+			break;
+
 
 		default:
 			console.log("option not found? : (");
-			break;
-	}
-	prefillGuageInputs(0);
+			break;		
+		}
+
+		(isOctave ? setForOctaveInput(): setForRegularInput());
+		prefillGuageInputs(0);
 }
 
 // fills the dropdown menu with possible sets
@@ -94,7 +66,7 @@ var fillStringSetList = function(stringSets)
 	}
 }
 
-// puts the string guages from the chosen set on page
+// puts the string guages from the chosen set as inputs
 var prefillGuageInputs = function(setIndex)
 {
 	chosenSet = currentSets[setIndex]	
@@ -103,65 +75,38 @@ var prefillGuageInputs = function(setIndex)
 	// clear the inputs
 	clearChildren("guage-input");
 
-	// set the number of strings field
-	document.getElementById("num-strings").value = numStrings;
-
 	// fill the guage input section
-	if(!isOctave())
+	for(var i = 0; i < numStrings; i++)
 	{
-		for(var i = 0; i < numStrings; i++)
+		var div = document.createElement("div");
+
+		var label = document.createElement("label");
+		label.innerHTML = chosenSet.strings[i].stringName + ": ";
+
+		var input = document.createElement("input");
+		input.classList.add("string-input");
+		input.value = chosenSet.strings[i].guage;
+		input.addEventListener("keyup", calculate);
+
+		div.appendChild(label);
+		div.appendChild(input);
+
+		if(isOctave)
 		{
-
-			var div = document.createElement("div");
-
-			var label = document.createElement("label");
-			label.innerHTML = chosenSet.strings[i].stringName + ": ";
-
-			var input = document.createElement("input");
-			input.classList.add("string-input");
-			input.value = chosenSet.strings[i].guage;
-			input.addEventListener("change", calculate);
-
-			div.appendChild(label);
-			div.appendChild(input);
-			document.getElementById("guage-input").appendChild(div);
-		}
-	}
-	else
-	{
-		var i = 0;
-		while(i < numStrings)
-		{
-			var div = document.createElement("div");
-
-			var label = document.createElement("label");
-			label.innerHTML = chosenSet.strings[i].stringName + ": ";
-
-			var input = document.createElement("input");
-			input.classList.add("string-input");
-			input.value = chosenSet.strings[i].guage;
-			input.addEventListener("change", calculate);
-			input.classList.add("inline");
-
 			i++;
 
-			var label2 = document.createElement("label");
-			label2.innerHTML = chosenSet.strings[i].stringName + ": ";
+			var label_2 = document.createElement("label");
+			label_2.innerHTML = chosenSet.strings[i].stringName + ": ";
 
-			var input2 = document.createElement("input");
-			input2.classList.add("string-input");
-			input2.value = chosenSet.strings[i].guage;
-			input2.addEventListener("change", calculate);
-			input2.classList.add("inline");
-
-			i++;
-
-			div.appendChild(label);
-			div.appendChild(input);
-			div.appendChild(label2);
-			div.appendChild(input2);
-			document.getElementById("guage-input").appendChild(div); 
+			var input_2 = document.createElement("input");
+			input_2.classList.add("string-input");
+			input_2.value = chosenSet.strings[i].guage;
+			input_2.addEventListener("keyup", calculate);
+			div.appendChild(label_2);
+			div.appendChild(input_2);
 		}
+
+		document.getElementById("guage-input").appendChild(div);
 	}
 
 	calculate();
@@ -173,14 +118,28 @@ var calculate = function()
 {
 	var stringGuageElements = document.getElementsByClassName("string-input");
 	var stringGuages = [];
-	var centerToCenterElement = document.getElementById("width");
-	var totalStringWidth = 0;
-	var spacing = null;
-	var numStrings = stringGuageElements.length;
 
+	var widthElement = document.getElementById("width");
+	var width;
+
+	var unionSpaceElem = document.getElementById("union-space");
+	var unionSpace;
+
+	// for calculations
+	var totalStringWidth = 0;
+	var spacing;
+	var numStrings = stringGuageElements.length;
+	var trebleSide;
+	var bassSide;
+	
 	// to flag if exception was thrown
 	var illegalStringInput = false;
-	var illegalCenterToCenter = false;
+	var illegalWidth = false;
+	var illegalUnionSpace = false;
+
+	// remove any previous errors
+	widthElement.classList.remove("error");
+	unionSpaceElem.classList.remove("error");
 
 	// add up all string guages
 	for(var i = 0; i < numStrings; i++)
@@ -190,22 +149,15 @@ var calculate = function()
 		{
 			var temp = parseFloat(stringGuageElements[i].value)
 			
+			// if OK
 			if(!isNaN(temp) && temp > 0)
 			{
 				totalStringWidth += temp;
 				stringGuages[i] = temp;
 			}
-			else if(isNaN(temp))
-			{
-				throw("Invalid Input - NaN")
-			}
-			else if(temp < 0)
-			{
-				throw("input was less than zero")
-			}
 			else
 			{
-				throw("input error");
+				throw("String Input Error");
 			}
 		}
 		catch(exception)
@@ -216,216 +168,224 @@ var calculate = function()
 		}
 	}
 
-
-	// dont continue if bad input
-	if(illegalStringInput)
+	// get union space if necessary
+	if(isOctave)
 	{
-		return;
+		try
+		{	
+			var temp = parseFloat(unionSpaceElem.value)
+			// if OK
+			if(!isNaN(temp) && temp > 0)
+			{
+				unionSpace = temp;
+				totalStringWidth += unionSpace*(numStrings/2);
+			}
+			else
+			{
+				throw("Union Spacing Error")
+			}
+		}
+		catch(exception)
+		{
+			console.log(exception);
+			unionSpaceElem.classList.add("error");
+			illegalUnionSpace = true;
+		}
 	}
 
-	// since we measure illegalCenterToCenter to center, ignore space beyond in total
-	totalStringWidth -= stringGuages[0]/2 + stringGuages[numStrings-1]/2;
-
+	// get nut width
 	try
 	{
-		// clear old data
-		clearChildren("output-table-body");
-		clearChildren("output-table-head");
+		var temp = parseFloat(widthElement.value);
 
-		var centerToCenter = parseFloat(centerToCenterElement.value);
-		console.log(centerToCenter);
-
-		if(isNaN(centerToCenter) || centerToCenter < totalStringWidth)
+		// if OK
+		if(!isNaN(temp) && temp > totalStringWidth)
 		{
-			throw("invalid center to center");
-		}
-		centerToCenterElement.classList.remove("error");
-
-		if(isOctave())
-		{
-			lastOctaveSpacing = parseFloat(document.getElementById("octave-spacing").value);
-			totalStringWidth += lastOctaveSpacing * (numStrings/2);
-		}
-
-		if(!isOctave())
-		{
-			spacing = (centerToCenter-totalStringWidth)/(numStrings-1);
+			width = temp;
 		}
 		else
 		{
-			spacing = (centerToCenter-totalStringWidth)/((numStrings/2)-1);
+			throw("Invalid Width: Not a number or more greater than sum of all strings.");
 		}
-	
-
-		var trebleSide = -(stringGuages[0]/2);
-		var bassSide = stringGuages[0]/2;
-
-		// for the table Head
-		var trHead = document.createElement("tr");
-		var stringCol = document.createElement("th");
-		var trebleCol = document.createElement("th");
-		var bassCol = document.createElement("th");
-
-		stringCol.innerHTML = "String";
-		trebleCol.innerHTML = "Treble";
-		bassCol.innerHTML = "Bass";
-
-		trHead.appendChild(stringCol);
-		trHead.appendChild(trebleCol);
-		trHead.appendChild(bassCol);
-		
-
-		if(!isOctave())
-		{
-			for (var i = 0; i < numStrings; i++)
-			{
-				var tr = document.createElement("tr");
-				var nameTd = document.createElement("td")
-				var trebleTd = document.createElement("td");
-				var bassTd = document.createElement("td")
-
-				nameTd.innerHTML = chosenSet.strings[i].stringName;;
-				trebleTd.innerHTML = trebleSide.toFixed(precision);
-				bassTd.innerHTML = bassSide.toFixed(precision);
-
-				tr.appendChild(nameTd);
-				tr.appendChild(trebleTd);
-				tr.appendChild(bassTd);
-
-				document.getElementById("output-table-body").appendChild(tr);
-
-				trebleSide += stringGuages[i] + spacing;
-				bassSide += spacing + stringGuages[i+1];
-			}
-		}
-		// if is an octave instrument
-		else
-		{	
-			// add columns to table for octave strings
-			var stringCol2 = document.createElement("th");
-			var trebleCol2 = document.createElement("th");
-			var bassCol2 = document.createElement("th");
-
-			stringCol2.innerHTML = "String"
-			trebleCol2.innerHTML = "Treble"
-			bassCol2.innerHTML = "Bass";
-
-			trHead.appendChild(stringCol2);
-			trHead.appendChild(trebleCol2);
-			trHead.appendChild(bassCol2);
-
-			var i = 0;
-			while(i < numStrings)
-			{
-				console.log("spacing: " + spacing)
-				console.log("lastOctaveSpacing: " + lastOctaveSpacing)
-
-				var tr = document.createElement("tr");
-				var nameTd = document.createElement("td")
-				var trebleTd = document.createElement("td");
-				var bassTd = document.createElement("td")
-				var name2Td = document.createElement("td");
-				var treble2Td = document.createElement("td");
-				var bass2Td = document.createElement("td")
-
-				// puts string in table
-				nameTd.innerHTML = chosenSet.strings[i].stringName;
-				trebleTd.innerHTML = trebleSide.toFixed(precision);
-				bassTd.innerHTML = bassSide.toFixed(precision);
-				tr.appendChild(nameTd);
-				tr.appendChild(trebleTd);
-				tr.appendChild(bassTd);
-
-				// sets the location for next string
-				trebleSide += stringGuages[i] + lastOctaveSpacing;
-				i++;
-				bassSide += lastOctaveSpacing + stringGuages[i];
-				
-				// puts mext stromg in the table
-				name2Td.innerHTML = chosenSet.strings[i].stringName;
-				treble2Td.innerHTML = trebleSide.toFixed(precision);
-				bass2Td.innerHTML = bassSide.toFixed(precision);
-				tr.appendChild(name2Td);
-				tr.appendChild(treble2Td);
-				tr.appendChild(bass2Td);
-
-				// sets location for next string
-				trebleSide += stringGuages[i] + spacing;
-				i++;
-				bassSide += spacing + stringGuages[i];
-
-				
-
-				// add table row
-				document.getElementById("output-table-body").appendChild(tr);
-			}
-		}
-
-		document.getElementById("output-table-head").appendChild(trHead);
 	}
 	catch(exception)
 	{
 		console.log(exception);
-		centerToCenterElement.classList.add("error");
+		widthElement.classList.add("error");
+		illegalWidth = true;
 	}
 
-}
 
-var makeCustomSet = function()
-{
-	var numStrings = document.getElementById("num-strings").value;
-	console.log("num Strings: " + numStrings);
-	var customStrings = [];
+	// clear old data
+	clearChildren("output-table-body");
+	clearChildren("output-table-head");
 
-	for(var i = 0; i < numStrings; i++)
+	if(illegalStringInput || illegalWidth || illegalUnionSpace)
 	{
-		customStrings.push(new InstrumentString(null, "String " + (i+1)));
+		return;
 	}
 
-	var set = [new InstrumentStringSet(customStrings, "Custom Set")];
-	fillStringSetList(set);
-	prefillGuageInputs(0);
-}
-
-var showOctaveSpacingInput = function()
-{
-	if(!octaveInputShowing)
+	// set bass/treble side starting points
+	if(isCenterToCenter())
 	{
-		var div = document.createElement("div");
-		div.id = "octave-input";
+		trebleSide = -(stringGuages[0]/2);
+		bassSide = stringGuages[0]/2;
 
-		var input = document.createElement("input");
-		input.id = "octave-spacing";
-		input.value = lastOctaveSpacing;
-		input.addEventListener("change", calculate);
+		totalStringWidth -= stringGuages[0]/2 + stringGuages[numStrings-1]/2;
+	}
+	else if (isEdgeToEdge())
+	{
+		trebleSide = 0;
+		bassSide = stringGuages[0];
+	}
 
+	// for the table Head
+	var trHead = document.createElement("tr");
+	var stringCol = document.createElement("th");
+	var trebleCol = document.createElement("th");
+	var bassCol= document.createElement("th");
+	var stringCol2 = document.createElement("th");
+	var trebleCol2 = document.createElement("th");
+	var bassCol2 = document.createElement("th");
+
+	stringCol.innerHTML = "";
+	trebleCol.innerHTML = "Treble Side";
+	bassCol.innerHTML = "Bass Side";
+
+	trHead.appendChild(stringCol);
+	trHead.appendChild(trebleCol);
+	trHead.appendChild(bassCol);
 		
-		var label = document.createElement("label");
-		label.innerHTML = "Space Between Octave Strings";
-
-		div.appendChild(label);
-		div.appendChild(input);
-
-		document.getElementById("options").appendChild(div);
-		octaveInputShowing = true;
-
-		prefillGuageInputs(0);
-	}
-}
-
-var removeOctaveSpacingInput = function()
-{
-	if(octaveInputShowing)
+	if(!isOctave)
 	{
-		var div = document.getElementById("octave-input");
-		div.parentNode.removeChild(div);
-		octaveInputShowing = false;
+		spacing = (width-totalStringWidth)/(numStrings-1);
+		for (var i = 0; i < numStrings; i++)
+		{
+			var tr = document.createElement("tr");
+			var nameTd = document.createElement("td")
+			var trebleTd = document.createElement("td");
+			var bassTd = document.createElement("td");
+			nameTd.innerHTML = chosenSet.strings[i].stringName + " (" + (i+1) + ")";
+			trebleTd.innerHTML = trebleSide.toFixed(precision);
+			bassTd.innerHTML = bassSide.toFixed(precision);
 
-		prefillGuageInputs(0);
+			tr.appendChild(nameTd);
+			tr.appendChild(trebleTd);
+			tr.appendChild(bassTd);
+
+			document.getElementById("output-table-body").appendChild(tr);
+
+			trebleSide += stringGuages[i] + spacing;
+			bassSide += spacing + stringGuages[i+1];
+		}
+	}
+	// if is an octave instrument
+	else
+	{	
+		// add table
+		stringCol2.innerHTML = "";
+		trebleCol2.innerHTML = "Treble Side";
+		bassCol2.innerHTML = "Bass Side";
+		trHead.appendChild(stringCol2);
+		trHead.appendChild(trebleCol2);
+		trHead.appendChild(bassCol2);
+
+		spacing = (width-totalStringWidth)/(numStrings/2-1)
+		var i = 0;
+		
+		// each loop does a unison
+		while(i < numStrings)
+		{
+			// the row
+			var tr = document.createElement("tr");
+
+			// the first
+			var nameTd_1 = document.createElement("td")
+			var trebleTd_1 = document.createElement("td");
+			var bassTd_1 = document.createElement("td")
+
+			nameTd_1.innerHTML = chosenSet.strings[i].stringName + " (" + (i+1) + ")";
+			trebleTd_1.innerHTML = trebleSide.toFixed(precision);
+			bassTd_1.innerHTML = bassSide.toFixed(precision);
+
+			tr.appendChild(nameTd_1);
+			tr.appendChild(trebleTd_1);
+			tr.appendChild(bassTd_1);
+
+			if(i%2 == 0)
+			{
+				trebleSide += stringGuages[i] + unionSpace;
+				bassSide += stringGuages[i+1] + unionSpace;
+			}
+			else
+			{
+				trebleSide += stringGuages[i] + spacing;
+				bassSide += stringGuages[i+1] + spacing;
+			}
+
+			i++;
+				
+			// the second string
+			var nameTd_2 = document.createElement("td")
+			var trebleTd_2 = document.createElement("td");
+			var bassTd_2 = document.createElement("td")
+
+			nameTd_2.innerHTML = chosenSet.strings[i].stringName + " (" + (i+1) + ")";
+			trebleTd_2.innerHTML = trebleSide.toFixed(precision);
+			bassTd_2.innerHTML = bassSide.toFixed(precision);
+			tr.appendChild(nameTd_2);
+			tr.appendChild(trebleTd_2);
+			tr.appendChild(bassTd_2);
+
+			document.getElementById("output-table-body").appendChild(tr);
+
+			if(i%2 == 0)
+			{
+				trebleSide += stringGuages[i] + unionSpace;
+				bassSide += stringGuages[i+1] + unionSpace;
+			}
+			else
+			{
+				trebleSide += stringGuages[i] + spacing;
+				bassSide += stringGuages[i+1] + spacing;
+			}
+				
+			i++;
+		}
+	}
+
+	document.getElementById("output-table-head").appendChild(trHead);
+}
+
+
+var isCenterToCenter = function()
+{
+	if(document.getElementById("center-to-center-radio").checked == true)
+	{
+		return true;
 	}
 }
+
+var isEdgeToEdge = function()
+{
+	if(document.getElementById("edge-to-edge-radio").checked == true)
+	{
+		return true;
+	}
+}
+
 
 var clearChildren = function(parentId)
 {
 	document.getElementById(parentId).innerHTML = "";
+}
+
+var setForOctaveInput = function()
+{
+	document.getElementById("union-space-wrapper").style.display = "inline";
+}
+
+var setForRegularInput = function()
+{
+	document.getElementById("union-space-wrapper").style.display = "none";
 }
--- js/sets.js
+++ js/sets.js
@@ -12,132 +12,148 @@ class InstrumentStringSet {
 		this.strings = strings;
 		this.setName = setName;
 	}
-
-	addString()
-	{
-
-	}
-
-	removeString()
-	{
-		
-	}
 }
 
 //----String guages----
 var acousticLightStrings = [
-	new InstrumentString(.012, "e"),
-	new InstrumentString(.016, "b"),
-	new InstrumentString(.024, "g"),
-	new InstrumentString(.032, "d"),
-	new InstrumentString(.042, "a"),
-	new InstrumentString(.053, "E")
+new InstrumentString(.012, "e"),
+new InstrumentString(.016, "b"),
+new InstrumentString(.024, "g"),
+new InstrumentString(.032, "d"),
+new InstrumentString(.042, "a"),
+new InstrumentString(.053, "E")
 ];
 
 var acousticMedStrings = [
-	new InstrumentString(.013, "e"),
-	new InstrumentString(.017, "b"),
-	new InstrumentString(.026, "g"),
-	new InstrumentString(.035, "d"),
-	new InstrumentString(.045, "a"),
-	new InstrumentString(.056, "E") 
+new InstrumentString(.013, "e"),
+new InstrumentString(.017, "b"),
+new InstrumentString(.026, "g"),
+new InstrumentString(.035, "d"),
+new InstrumentString(.045, "a"),
+new InstrumentString(.056, "E") 
 ];
 
 var electricExtraLightStrings = [
-	new InstrumentString(.009, "e"),
-	new InstrumentString(.011, "b"),
-	new InstrumentString(.016, "g"),
-	new InstrumentString(.024, "d"),
-	new InstrumentString(.032, "a"),
-	new InstrumentString(.042, "E") 
+new InstrumentString(.009, "e"),
+new InstrumentString(.011, "b"),
+new InstrumentString(.016, "g"),
+new InstrumentString(.024, "d"),
+new InstrumentString(.032, "a"),
+new InstrumentString(.042, "E") 
 ];
 
 var electricLightStrings = [
-	new InstrumentString(.010, "e"),
-	new InstrumentString(.013, "b"),
-	new InstrumentString(.017, "g"),
-	new InstrumentString(.026, "d"),
-	new InstrumentString(.036, "a"),
-	new InstrumentString(.046, "E") 
+new InstrumentString(.010, "e"),
+new InstrumentString(.013, "b"),
+new InstrumentString(.017, "g"),
+new InstrumentString(.026, "d"),
+new InstrumentString(.036, "a"),
+new InstrumentString(.046, "E") 
 ];
 
 var electricMedStrings = [
-	new InstrumentString(.011, "e"),
-	new InstrumentString(.014, "b"),
-	new InstrumentString(.018, "g"),
-	new InstrumentString(.028, "d"),
-	new InstrumentString(.038, "a"),
-	new InstrumentString(.048, "E") 
+new InstrumentString(.011, "e"),
+new InstrumentString(.014, "b"),
+new InstrumentString(.018, "g"),
+new InstrumentString(.028, "d"),
+new InstrumentString(.038, "a"),
+new InstrumentString(.048, "E") 
 ];
 
 var bassLightStrings = [
-	new InstrumentString(.045, "G"),
-	new InstrumentString(.065, "D"),
-	new InstrumentString(.080, "A"),
-	new InstrumentString(.100, "E")
+new InstrumentString(.045, "G"),
+new InstrumentString(.065, "D"),
+new InstrumentString(.080, "A"),
+new InstrumentString(.100, "E")
 ];
 
 var bassMedStrings = [
-	new InstrumentString(.050, "G"),
-	new InstrumentString(.070, "D"),
-	new InstrumentString(.085, "A"),
-	new InstrumentString(.105, "E")
+new InstrumentString(.050, "G"),
+new InstrumentString(.070, "D"),
+new InstrumentString(.085, "A"),
+new InstrumentString(.105, "E")
 ];
 
 var mandoLightStrings = [
-	new InstrumentString(.010, "e"),
-	new InstrumentString(.010, "e"),
-	new InstrumentString(.014, "a"),
-	new InstrumentString(.014, "a"),
-	new InstrumentString(.024, "d"),
-	new InstrumentString(.024, "d"),
-	new InstrumentString(.038, "g"),
-	new InstrumentString(.038, "g")
-]
+new InstrumentString(.010, "e"),
+new InstrumentString(.010, "e"),
+new InstrumentString(.014, "a"),
+new InstrumentString(.014, "a"),
+new InstrumentString(.024, "d"),
+new InstrumentString(.024, "d"),
+new InstrumentString(.038, "g"),
+new InstrumentString(.038, "g")
+];
 
 var mandoMedLightStrings = [
-	new InstrumentString(.011, "e"),
-	new InstrumentString(.011, "e"),
-	new InstrumentString(.015, "a"),
-	new InstrumentString(.015, "a"),
-	new InstrumentString(.024, "d"),
-	new InstrumentString(.024, "d"),
-	new InstrumentString(.038, "g"),
-	new InstrumentString(.038, "g")
-]
+new InstrumentString(.011, "e"),
+new InstrumentString(.011, "e"),
+new InstrumentString(.015, "a"),
+new InstrumentString(.015, "a"),
+new InstrumentString(.024, "d"),
+new InstrumentString(.024, "d"),
+new InstrumentString(.038, "g"),
+new InstrumentString(.038, "g")
+];
 
 var mandoMedStrings = [
-	new InstrumentString(.011, "e"),
-	new InstrumentString(.011, "e"),
-	new InstrumentString(.015, "a"),
-	new InstrumentString(.015, "a"),
-	new InstrumentString(.026, "d"),
-	new InstrumentString(.026, "d"),
-	new InstrumentString(.040, "g"),
-	new InstrumentString(.040, "g")
-]
+new InstrumentString(.011, "e"),
+new InstrumentString(.011, "e"),
+new InstrumentString(.015, "a"),
+new InstrumentString(.015, "a"),
+new InstrumentString(.026, "d"),
+new InstrumentString(.026, "d"),
+new InstrumentString(.040, "g"),
+new InstrumentString(.040, "g")
+];
 
 var mandoHeavyMedStrings = [
-	new InstrumentString(.0115, "e"),
-	new InstrumentString(.0115, "e"),
-	new InstrumentString(.016, "a"),
-	new InstrumentString(.016, "a"),
-	new InstrumentString(.026, "d"),
-	new InstrumentString(.026, "d"),
-	new InstrumentString(.041, "g"),
-	new InstrumentString(.041, "g")
+new InstrumentString(.0115, "e"),
+new InstrumentString(.0115, "e"),
+new InstrumentString(.016, "a"),
+new InstrumentString(.016, "a"),
+new InstrumentString(.026, "d"),
+new InstrumentString(.026, "d"),
+new InstrumentString(.041, "g"),
+new InstrumentString(.041, "g")
 
-]
+];
 
+var twelveStringAcousticLightStrings = [
+new InstrumentString(.010, "e"),
+new InstrumentString(.010, "e"),
+new InstrumentString(.014, "b"),
+new InstrumentString(.014, "b"),
+new InstrumentString(.023, "g"),
+new InstrumentString(.008, "g"),
+new InstrumentString(.030, "d"),
+new InstrumentString(.012, "d"),
+new InstrumentString(.039, "a"),
+new InstrumentString(.018, "a"),
+new InstrumentString(.047, "E"),
+new InstrumentString(.027, "E")
 
+];
 
-var mandoEZStrings = [
-	new InstrumentString(1, "1"),
-	new InstrumentString(1, "2"),
-	new InstrumentString(1, "3"),
-	new InstrumentString(1, "4"),
-]
+var tweleveStringElecLightStrings = [
+new InstrumentString(.010, "e"),
+new InstrumentString(.010, "e"),
+new InstrumentString(.013, "b"),
+new InstrumentString(.013, "b"),
+new InstrumentString(.017, "g"),
+new InstrumentString(.008, "g"),
+new InstrumentString(.026, "d"),
+new InstrumentString(.012, "d"),
+new InstrumentString(.036, "a"),
+new InstrumentString(.018, "a"),
+new InstrumentString(.046, "E"),
+new InstrumentString(.026, "E")
 
+];
+
+//----12 String Sets----
+var twelveStringAcousticLight = new InstrumentStringSet(twelveStringAcousticLightStrings, "Acoustic - Light");
+var tweleveStringElectricLight = new InstrumentStringSet(tweleveStringElecLightStrings, "Electric - Light");
 
 //----Guitar Sets----
 var acousticLight = new InstrumentStringSet(acousticLightStrings, "Acoustic - Light");
@@ -160,64 +176,28 @@ var mandoHeavyMed = new InstrumentStringSet(mandoHeavyMedStrings, "Heavy-Medium"
 var customSet = new InstrumentStringSet([], "")
 
 //---Arrays of sets----
+
+var twelveStringSets = [
+twelveStringAcousticLight,
+tweleveStringElectricLight
+
+];
+
 var guitarSets = [
-		acousticLight, 
-		acousticMed, 
-		electricExtraLight, 
-		electricLight, 
-		electricMed];
+acousticLight, 
+acousticMed, 
+electricExtraLight, 
+electricLight, 
+electricMed];
 
 var bassSets = [
-		bassLight, 
-		bassMed];
+bassLight, 
+bassMed
+];
 
 var mandoSets = [
-		mandoLight,
-		mandoMedLight,
-		mandoMed,
-		mandoHeavyMed
-		]
-
-
-var custSets = [
-	customSet
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var bassLight = {
-	strings: [
-	new InstrumentString(.30, "g"),
-	new InstrumentString(.040, "d"),
-	new InstrumentString(.050, "a"),
-	new InstrumentString(.060, "E") ],
-
-	name: "Light"
-};
-
-var bassMed = {
-	strings: [
-	new InstrumentString(.035, "g"),
-	new InstrumentString(.045, "d"),
-	new InstrumentString(.055, "a"),
-	new InstrumentString(.065, "E") ],
-
-	name: "Medium"
-};
-
-var custom = {
-	strings: [],
-	name: "custom",
-}
+mandoLight,
+mandoMedLight,
+mandoMed,
+mandoHeavyMed
+];
